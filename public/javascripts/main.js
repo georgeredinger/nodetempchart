ba7f7242e4a0bc2ab9312e63bd00662815fe6f7f
George Redinger <george@georgeredinger.com>
2013-01-15 09:18:41 -0800

    add last sample time  to client

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,14 +2,23 @@ var socket=io.connect(), d1=[], batt=[], zone_delta=(new Date()).getTimezoneOffs
 var limit=50000; 
 socket.on('newdata', function(v) {
 	var ts=v[0]-zone_delta;
+	var box = '<td class="legendColorBox"><div style="border:1px solid  white ;padding:1px><div style="width:4px;height:0;border:5px solid  black;overflow:hidden"></div></div></td>'
+
+  var d = new Date();
 	d1.push([ts, v[1]]);	
 	batt.push([ts, v[2]]);	
 	re_flot();	
 	var i=1;
 	$('#legend').find('tr').each(function() {
-		$(this).append('<td class="last_val">'+v[i++]+'</td>');
+		$(this).append('<td class="last_val">'+Math.round(v[i++]*100,3)/100+'</td>');
 	});
+ d.getHours();
+ d.getMinutes();
+	$('#legend').find('table').append('<tr>'+box+'<td>Last Sample at:</td> <td>'+d.getHours()+':'+d.getMinutes()+'</td></tr>');
 });
+
+
+
 socket.on('history', function(a) {
 	for(var i=0, l=a.length;i<l;i++) {
 		var v=a[i],  ts=v[0]-zone_delta;
@@ -51,8 +60,8 @@ function re_flot() {
 			//xaxis:{mode:'time', timeFormat:'%h:%M', tickSize:[tick_int, "second"]},
 			xaxis:{mode:'time', timeFormat:'%h', tickSize:[1, "hour"]},
 			// outside yaxis: {min:-12, max: 102,  tickSize: 10}, 
-			yaxis: {min:55, max: 85,  tickSize: 2}, //inside
-			y2axis: {min:3.3, max: 4.3,  tickSize: 0.1}, 
+			yaxis: {min:0, max: 100,  tickSize: 5}, //inside
+			y2axis: {min:3.4, max: 4.2,  tickSize: 0.1}, 
 			legend: { container: $('#legend') }
 		}
 	);
